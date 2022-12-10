450428950bf32e108ac1305150a0794f46b11441
Marta Taszmowicz <marta.taszmowicz@gmail.com>
2022-12-10 23:05:42 +0100

    New idea of gameplay

---
+++ Assets/Marta/PlantController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantController : MonoBehaviour
+{
+    public PlanetService planetService;
+    public float angel;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        planetService.pointAround = this.gameObject.transform.position;
+        angel = angel * -1;
+    }
+    
+
+    void FixedUpdate () {
+    
+        Vector3 axis = new Vector3(0,0,1);
+        transform.RotateAround(planetService.pointAround, axis, angel /*Time.deltaTime * 10*/);
+    }
+}
